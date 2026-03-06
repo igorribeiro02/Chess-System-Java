9367f9eff45c5bfb6b5d209e729dd23c8d29496e
igor Lopes Ribeiro <117321736+igorribeiro02@users.noreply.github.com>
2026-03-06 10:39:49 -0300

    adicionando possiveis movimentações do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,38 +9,37 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
-	
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-		// TODO Auto-generated method stub
-		while(true) {
+		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces()); // vai receber a matriz de peças do chessmatch
+				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
 				System.out.print("Source: ");
-				ChessPosition source = UI.readChessPosition(sc); // vai ler a posição do xadrez, ou seja, a letra e o número, e retornar a posição do xadrez
-			
-				System.out.print("Target: ");
-				ChessPosition target = UI.readChessPosition(sc); // vai ler a posição do xadrez, ou seja, a letra e o número, e retornar a posição do xadrez
+				ChessPosition source = UI.readChessPosition(sc);
 				
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);// vai receber a peça capturada, ou seja, a peça que foi movida para a posição de destino
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			}
-			catch (ChessException e) { // caso ocorra alguma exceção, ou seja, caso ocorra algum erro, como por exemplo, se o usuário digitar uma posição inválida, ou se o usuário tentar mover uma peça para uma posição inválida, ou se o usuário tentar mover uma peça que não existe, ou se o usuário tentar mover uma peça que não tem movimentos possíveis, ou seja, caso ocorra algum erro, a exceção vai ser capturada e a mensagem de erro vai ser exibida para o usuário
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			catch (InputMismatchException e) { // caso ocorra alguma exceção, ou seja, caso ocorra algum erro, como por exemplo, se o usuário digitar uma posição inválida, ou se o usuário tentar mover uma peça para uma posição inválida, ou se o usuário tentar mover uma peça que não existe, ou se o usuário tentar mover uma peça que não tem movimentos possíveis, ou seja, caso ocorra algum erro, a exceção vai ser capturada e a mensagem de erro vai ser exibida para o usuário
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 		}
-
-		
-
 	}
-
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,7 +7,7 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-	
+
 	private Board board;
 	
 	public ChessMatch() {
@@ -15,57 +15,57 @@ public class ChessMatch {
 		initialSetup();
 	}
 	
-	public ChessPiece[][] getPieces(){
+	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for(int i=0; i<board.getRows(); i++) {
-			for(int j=0; j<board.getColumns(); j++) {
+		for (int i=0; i<board.getRows(); i++) {
+			for (int j=0; j<board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
 		return mat;
 	}
-
-    // CORREÇÃO: O tipo de retorno deve ser ChessPiece (a peça capturada)
-    // Os parâmetros devem ser ChessPosition
+	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		Position source = sourcePosition.toPosition(); 
+		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
-		validateSourcePosition(source); // Nome do método corrigido
-		validateTargetPosition(source, target); // Método para validar a posição de destino (a ser implementado)
+		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
-		return (ChessPiece) capturedPiece; 
+		return (ChessPiece)capturedPiece;
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source); 
-		Piece capturedPiece = board.removePiece(target); 
-		board.placePiece(p, target); 
-		return capturedPiece; 
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
 	}
 	
-    // CORREÇÃO: Nome ajustado para validateSourcePosition (estava validadete...)
 	private void validateSourcePosition(Position position) {
-		if(!board.thereIsAPiece(position)) {
-			throw new ChessException("Não existe peça na posição de origem.");
+		if (!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
 		}
-        // Este método 'possibleMoves' deve estar implementado na classe Piece
-		//se nao tiver nenhum movimento possível, ou seja, se a peça não tiver nenhum movimento possível, lança uma exceção
-		if(!board.piece(position).isThereAnyPossibleMove()) {
-			//lanço uma excessão, ou seja, se a peça não tiver nenhum movimento possível, lança uma exceção
-			throw new ChessException("Não existem movimentos possíveis para a peça escolhida.");
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
 		}
 	}
 	
 	private void validateTargetPosition(Position source, Position target) {
-		if(!board.piece(source).possibleMove(target)) {
-			throw new ChessException("A peça escolhida não pode se mover para a posição de destino.");
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
-		board.placePiece(piece, new ChessPosition(column, row).toPosition()); 
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-
+	
 	private void initialSetup() {
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
